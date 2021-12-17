ab88a26fa92d9efd2b117bd40ba6e0e80767007c
Viktor Syrytsia <syrytsiaviktor@gmail.com>
2021-12-17 18:49:10 +0000

    added tailwind & configured

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+</script>
+
+<h1 class="font-bold text-3xl p-4 text-center">Welcome to SvelteKit</h1>
+<p class="w-1/4 p-4 shadow-lg bg-green-50 font-semibold text-center mx-auto">
+    Visit <a
+        class="bg-yellow-400 text-white p-1 rounded-md"
+        href="https://kit.svelte.dev">kit.svelte.dev</a
+    > to read the documentation
+</p>
