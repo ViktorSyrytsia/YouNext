50dcdcdfec9b651702692f025caecf89b5ee7ac4
Viktor Syrytsia <syrytsiaviktor@gmail.com>
2021-12-31 15:43:04 +0000

    create team page done

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,7 +18,7 @@
     
     /* ================================ BUTTONS================================ */
     :global(.filled--primary) {
-        @apply mr-2 bg-teal-500 rounded-md text-white font-semibold px-3 py-2 capitalize hover:bg-teal-600 transition-colors focus:outline-none focus:ring focus:ring-teal-100;
+        @apply mr-2 bg-teal-500 rounded-md text-white font-semibold px-3 py-2 capitalize hover:bg-teal-600 transition-colors focus:outline-none focus:ring focus:ring-teal-100 disabled:bg-gray-200;
     }
     :global(.no-bg--primary) {
         @apply mr-2 rounded-md text-teal-500 font-semibold px-3 py-2 capitalize hover:text-teal-600 transition-colors focus:outline-none focus:ring focus:ring-teal-100;
--- src/routes/teams-create.svelte
+++ src/routes/teams-create.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    let teamName: string = 'Best team ever!'
+    let teamName: string = "";
     let memberCounter = 0;
     let members: number[] = [];
 
@@ -9,9 +9,6 @@
         members.forEach((element) => {
             teamMembers.push(e.target[element].value);
         });
-        console.log(teamname);
-        console.log(teamMembers);
-
         // postData("/api/teams.json", { name: "Fist Team" }).then((res) => {
         //     console.log({ res });
         // });
@@ -19,9 +16,14 @@
     const addMember = () => {
         members = [...members, ++memberCounter];
     };
+    const removeMember = (i: number) => {
+        members = members.filter((_, index) => index !== i);
+    };
 </script>
 
-<h1 class="text-5xl font-bold mt-20 p-6 text-center text-gray-800">Create new team!</h1>
+<h1 class="text-5xl font-bold mt-20 p-6 text-center text-gray-800">
+    Create new team!
+</h1>
 <h2 class="text-4xl font-bold text-center text-teal-500">{teamName}</h2>
 <div class="flex justify-center">
     <form
@@ -30,12 +32,29 @@
     >
         <div class="input-wrapper">
             <label for="name">Enter team name:</label>
-            <input on:input={e => teamName = e.target.value} placeholder="My Lovely team" type="text" name="name" />
+            <input
+                on:input={(e) => (teamName = e.target.value)}
+                placeholder="My Lovely team"
+                type="text"
+                name="name"
+            />
         </div>
-        {#each members as _, index}
+        {#each members as member, index (member)}
             <div class="input-wrapper">
                 <label for="name">Enter member name:</label>
-                <input placeholder="John Doe" type="text" name={`${index}`} />
+                <div class="flex">
+                    <input
+                        class="grow"
+                        placeholder="John Doe"
+                        type="text"
+                        name={`${index}`}
+                    />
+                    <button
+                        on:click|preventDefault={() => removeMember(index)}
+                        class="filled--primary"
+                        style="margin-right: 0px; margin-left: 8px">-</button
+                    >
+                </div>
             </div>
         {/each}
         <div class="flex justify-center mb-10 mt-5">
@@ -45,7 +64,7 @@
         </div>
 
         <div>
-            <button class="filled--primary">Create team</button>
+            <button disabled={!(teamName && members.length > 0)} class="filled--primary">Create team</button>
             <button class="no-bg--primary">Back</button>
         </div>
     </form>
